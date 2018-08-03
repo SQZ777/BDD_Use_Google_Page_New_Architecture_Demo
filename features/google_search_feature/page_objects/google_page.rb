b3d54faf74842b532dac0cfd83e75c6a28be2c81
Darren Zhang <darrenzhang@ip-10-11-90-161.ap-northeast-1.compute.internal>
2018-08-03 14:41:21 +0800

    use global

--- features/google_search_feature/page_objects/google_page.rb
+++ features/google_search_feature/page_objects/google_page.rb
@@ -4,12 +4,10 @@ require_relative "./ios/ios_google_implements.rb"
 require_relative "../../base/platform.rb"
 
 class GooglePage
-  def initialize(platform)
-    @platform = platform
-    extend DesktopGoogleImplements if @platform.platform.eql? "desktop"
-    extend AndroidGoogleImplements if @platform.platform.eql? "android"
-    extend IosGoogleImplements if @platform.platform.eql? "iOS"
-    puts @platform.platform
+  def initialize
+    extend DesktopGoogleImplements if $start_up_platform.platform.eql? "desktop"
+    extend AndroidGoogleImplements if $start_up_platform.platform.eql? "android"
+    extend IosGoogleImplements if $start_up_platform.platform.eql? "iOS"
   end
 
   def go_to_this_page
